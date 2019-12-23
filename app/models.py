d50cc3b5a30acd01ecbcfddc2356ed2a8f02e1a3
HermessNRJ <remi.boidet@gmail.com>
2019-12-23 12:28:48 +0100

    forms + supp double + rangement

--- app/models.py
+++ app/models.py
@@ -13,6 +13,6 @@ class DevisForm(FlaskForm):
     prenom = StringField('Prénom', validators=[DataRequired()],render_kw={"placeholder": "Prénom"})
     email = EmailField('Adresse mail',[validators.DataRequired(), validators.Email()],render_kw={"placeholder": "Email"})
     tel = StringField(render_kw={"placeholder": "Téléphone"})
-    description = StringField('Parlez nous de votre projet',widget=TextArea(),render_kw={"placeholder": "Parlez nous de votre projet"})
-    file = FileField(validators=[FileRequired()],render_kw={"placeholder": "Ajouter !"},label="Ajouter")
+    description = StringField('Parlez-nous de votre projet',widget=TextArea(),render_kw={"placeholder": "Parlez nous de votre projet"})
+    file = FileField(validators=[FileRequired()],render_kw={"placeholder": "Ajouter un fichier"},label="Ajouter")
     submit = SubmitField('Envoyer !',render_kw={"placeholder": "Envoyer !"})
--- app/views.py
+++ app/views.py
@@ -28,10 +28,26 @@ def base():
 
     return render_template('static_oldsite.html',form=form, allowed=app.config['ALLOWED_EXTENSIONS'])
 
+@app.route('/process', methods = ['GET', 'POST'])
+def base():
+    form = DevisForm()
+    if request.method == 'POST':
+
+
+            if form.file.data:
+                file = request.files['file']
+                if file and allowed_file(file.filename):
+                    filename = secure_filename(file.filename)
+                    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                    flash('file uploaded successfully')
+                    return redirect(url_for('test'))
+
+                else:
+                    if file and not allowed_file(file.filename):
+                        flash("Un fichier de type autorisé svp")
+
+    return render_template('static_oldsite_process.html',form=form, allowed=app.config['ALLOWED_EXTENSIONS'])
+
 @app.route('/test')
 def test():
     return render_template('index.html')
-
-@app.route('/process')
-def process():
-    return render_template('static_oldsite_process.html')
